7ddc5b2c15e46c7e5af27e49dbfafa0e4efbb126
Jeffry <jeffryfleischer@gmail.com>
2020-06-30 19:03:34 -0700

    add function to remove list items to Goodbye and 'stop talking to me' buttons

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,22 @@ $(document).ready(function() {
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
